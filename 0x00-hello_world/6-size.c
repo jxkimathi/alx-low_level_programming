a52298fea21831b87c5744769e2ab5f58f1bd249
jxkimathi <judescriva8@gmail.com>
2023-02-16 10:12:17 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,12 @@
  */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(float));
-	printf("%lu\n", sizeof(double));
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The suze of a char is: %lu.\n", (unsigned long)sizeof(c));
 	return (0);
+}
