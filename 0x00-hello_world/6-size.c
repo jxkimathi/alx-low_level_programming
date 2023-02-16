04eb73a63e6978ff3d5e706de64d6185be350ab4
jxkimathi <judescriva8@gmail.com>
2023-02-16 10:05:23 -0800

    Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the various types of sizes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	return (0);
