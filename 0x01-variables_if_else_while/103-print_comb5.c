24c909ff17a9056d3e95ee992db4f12cf669fea0
jxkimathi <judescriva8@gmail.com>
2023-02-17 17:05:10 -0800

    Software is eating the world

---
+++ 0x01-variables_if_else_while/103-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Combinations of two digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0'); /* print first digit of i */
+			putchar((i % 10) + '0'); /* print second digit of i */
+			putchar(' ');
+			putchar((j / 10) + '0'); /* print first digit of j */
+			putchar((j % 10) + '0'); /*  print second digit of j */
+			if (i != 98 || j != 99) /* check if it's the last combination */
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
