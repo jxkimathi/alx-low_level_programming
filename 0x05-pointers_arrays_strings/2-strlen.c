b579c3b153a945c78571f65fddc81c8ff3fc2ce9
jxkimathi <judescriva8@gmail.com>
2023-02-27 05:47:21 -0800

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,16 +4,15 @@
  * _strlen - returns the length of a string
  * @s: string to be returned
  *
- * Return: Always 0
+ * Return: Length
  */
 int _strlen(char *s)
 {
-	int c = 0;
+	int length = 0;
 
-	for (; *s != '\0'; s++)
+	while (s[length])
 	{
-		c++;
+		length++;
 	}
-
-	return (0);
+	return (length);
 }
