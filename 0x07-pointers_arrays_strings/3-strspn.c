9f4c46a93f41dfec89d025e63d0c4819cf61998e
jxkimathi <judescriva8@gmail.com>
2023-03-06 11:59:04 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -23,14 +23,14 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 
-			else if (accept[i] + 1 == '\0')
+			else if (accept[i + 1] == '\0')
 			{
 				return (bytes);
 			}
 		}
-		
+
 		s++;
 	}
-	
+
 	return (bytes);
 }
