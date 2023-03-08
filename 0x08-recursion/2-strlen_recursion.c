2795475c934f0ad9f9896631a01954b915479177
jxkimathi <judescriva8@gmail.com>
2023-03-08 04:26:49 -0800

    strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,18 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to be returned
+ *
+ * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	
-		return (1 + _strlen_recursion(s + 1));
-	
-	else
-		return (0);
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+
+	return (length);
 }
