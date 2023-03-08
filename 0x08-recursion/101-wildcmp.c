5751edf91e274783cc22bdf52e9b3ec7125eb15e
jxkimathi <judescriva8@gmail.com>
2023-03-08 08:46:24 -0800

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 if identical; 0 if not identical
  */
-int wildcmp(char *s1, char*s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char*s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 +1) || wildcmp(s1 + 1, s2));
-	
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
 	return (0);
 }
