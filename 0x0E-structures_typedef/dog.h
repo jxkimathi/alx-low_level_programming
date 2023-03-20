1635d2dba6072a440ce851d8f51a8a9d49d1bd30
jxkimathi <judescriva8@gmail.com>
2023-03-20 13:23:11 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,27 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - A dog's basic info
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
  */
 
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 /**
- * dog_t - Typedef for the struct dog
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
