db6088b3c67b31e2b7daa0b7c11a634264abcaf9
jxkimathi <judescriva8@gmail.com>
2023-05-08 14:40:43 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	o = open(filename, O_CREAT | O _RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, length);
 
 	if (o == -1 || w == -1)
